31f5eda787fd6eaac5e754881bc544824c578aca
Christian Saaro <christian@saaro.de>
2025-01-19 22:18:01 +0100

    init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,52 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import AstroPWA from '@vite-pwa/astro'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://csaaro.github.io',
+    base: 'lineup-demo',
+  integrations: [react(),
+    AstroPWA({
+        mode: 'development',
+        base: 'lineup-demo/',
+        scope: '/lineup-demo/',
+        includeAssets: ['favicon.svg'],
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Astro PWA',
+          short_name: 'Astro PWA',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+        workbox: {
+          navigateFallback: '/',
+          globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+        },
+        devOptions: {
+          enabled: true,
+          navigateFallbackAllowlist: [/^\//],
+        },
+        experimental: {
+          directoryAndTrailingSlashHandler: true,
+        }
+      }),
+  ],
+});
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface LoginFormProps {}
+
+const LoginForm: React.FC<LoginFormProps> = () => {
+  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Verhindert den Standard-Submit
+    const username = (event.target as HTMLFormElement).username.value;
+    const password = (event.target as HTMLFormElement).password.value;
+
+    if (username === 'Demo' && password === 'Demo') {
+      window.location.href = '/dashboard';
+    } else {
+      alert('Ungültige Zugangsdaten!');
+    }
+  };
+
+  return (
+    <form method="post" onSubmit={handleLogin}>
+      <label htmlFor="username">Benutzername:</label>
+      <input type="text" id="username" name="username" required />
+
+      <label htmlFor="password">Passwort:</label>
+      <input type="password" id="password" name="password" required />
+
+      <button type="submit">Anmelden</button>
+    </form>
+  );
+};
+
+export default LoginForm;
